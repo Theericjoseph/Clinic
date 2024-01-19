c0e811d4d665a1a0d6f924f1cb37f6f88f149d96
Eric Joseph <ericjoseph2003@gmail.com>
2024-01-19 21:06:01 +1000

    Update

--- js/common.js
+++ js/common.js
@@ -26,5 +26,6 @@ window.addEventListener("load", () => {
 
 function logout() { // Clears session storage and logs out
     sessionStorage.clear();
+    document.cookie = 'token' + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
     window.location.href = '/login';
 }
